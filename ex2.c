4a32eb6d7095ec75fb79427a38c5ffc4b1b76da9
Paris Mollo <parismollochristondis@gmail.com>
2022-03-01 14:50:09 +0100

    exercice 2.2

--- ex2.c
+++ ex2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <assert.h>
 
 
 typedef struct array {
@@ -12,18 +13,27 @@ typedef struct array {
 array* array_init(unsigned int cap) {
     array* a = malloc(sizeof(array));
     if(a==NULL) {perror("Error...;("); exit(1);}
-    // assert sinon ou retourne -1
+    // assert sinon on retourne -1
     a -> ptr = malloc(cap*sizeof(int));
     if (a==NULL) {perror("Error...;("); exit(1);}
-    // assert sinon ou retourne -1
+    // assert sinon on retourne -1
     a -> capacite = cap;
     a -> taille = 0;
     return a;
+}
+
+void array_destroy(array *t) {
+    printf("\nCleaning memory allocation...");
+    assert(t!=NULL);
+    free(t->ptr);
+    free(t);
+    printf("Memory is clean.");
 }
 
 int main () {
     array* x = array_init(10);
     printf("\n[LOG]: Exercice 2.1- > x*: %p", x);
     printf("\n[LOG]: x*.capacite=%d; x*.taille=%d; x*.ptr=%p", x->capacite, x->taille, x->ptr);
+    array_destroy(x);
     return EXIT_SUCCESS;
 }
