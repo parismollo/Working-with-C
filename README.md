d3f0709fcecd8775a7fcdd73f1110bbd5e1fc69d
Paris Mollo <parismollochristondis@gmail.com>
2022-03-01 15:09:17 +0100

    exercice 2.3

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
 # TP4
-
-## Exercice 1
-1. Dans le premier cas, on passe l'addresse du premier élément du tableau au pointeur pt. Le deuxième cas c'est pareil. 
-Les deux autres cas sont pareil aussi. 
-
-2. 
--- ex2.c
+++ ex2.c
@@ -30,10 +30,25 @@ void array_destroy(array *t) {
     printf("Memory is clean.");
 }
 
+int array_get(array *t, unsigned int index) {
+    assert(t!=NULL); // Check if array structure exists
+    assert(index<=t->capacite); // Check if index is inside array defined
+    return t->ptr[index];
+}
+void array_set(array *t, unsigned int index, int valeur) {
+    assert(t!=NULL); // Check if array structure exists
+    assert(t->capacite>=index); // Check if position has something
+    t->taille = t->taille + 1; 
+    t->ptr[index] = valeur;
+}
+
+
 int main () {
     array* x = array_init(10);
     printf("\n[LOG]: Exercice 2.1- > x*: %p", x);
     printf("\n[LOG]: x*.capacite=%d; x*.taille=%d; x*.ptr=%p", x->capacite, x->taille, x->ptr);
-    array_destroy(x);
+    array_set(x, 2, 36);
+    printf("\nArray get: %d", array_get(x, 2));
+    // array_destroy(x);
     return EXIT_SUCCESS;
 }
